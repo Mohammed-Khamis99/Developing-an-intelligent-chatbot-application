5638fb3bc17b476be762953a54b71c01d00d42c6
Mohammed Khamis <mo.makein@gmail.com>
2025-06-09 19:22:17 +0300

    Update aws-exports.ts

--- frontend/src/configs/aws-exports.ts
+++ frontend/src/configs/aws-exports.ts
@@ -1,6 +1,6 @@
 export const amplifyConfig = {
-  aws_project_region: 'AWS_REGION_CODE',
-  aws_cognito_region: 'AWS_REGION_CODE',
-  aws_user_pools_id: 'CognitoUserPoolId_OUTPUT',
-  aws_user_pools_web_client_id: 'ClientId_OUTPUT',
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_mFkl6h1HV',
+  aws_user_pools_web_client_id: '3irlvi2g1fisbr5dlbb6ckb61d',
   };
